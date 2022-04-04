af505b7eb45874bbd5466c032332568951f1a6aa
Serje <Serje.havandjian@gmail.com>
2022-04-04 17:03:07 -0400

    commit tree db137193ea0c4acccb9995257e6fe97d8295bec3

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue <= 400){
+    result = "This one is on me!"
+  } else if (someValue >= 400 && someValue <= 2000){
+    result = "That will be twenty bucks."
+  } else if (someValue >2000 && someValue <2500){
+    result = "I will gladly take your thirty bucks."
+  }else {
+    result = "No can do."
+  }
+
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result
+
+  city == "NYC" ? (result = "Ok, sounds good.") : (result = "No go.")
+
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  
+  switch(tip){
+    case "generous":
+      return ("Thank you so much.");
+    case "not as generous":
+      return ("Thank you.");
+    default:
+      return ("Bye.")
+  }
 }
